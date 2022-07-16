8a1813f3c1813952071d7d03961a46fc3d749e91
Kumar <krbhaskar4321@gmail.com>
2022-07-16 19:47:02 +0530

    manage profile using firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+
 import 'rsuite/dist/styles/rsuite-default.css';
-import './style/main.scss';
+import './styles/main.scss';
+import { Switch } from 'react-router';
+import SignIn from './pages/SignIn';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
+import { ProfileProvider } from './context/profile.context';
+
 function App() {
   return (
-    <div >
-      hello
-    </div>
+    <ProfileProvider>
+      <Switch>
+        <PublicRoute path="/signin">
+          <SignIn />
+        </PublicRoute>
+        <PrivateRoute path="/">
+          <Home />
+        </PrivateRoute>
+      </Switch>
+    </ProfileProvider>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+
+/* eslint-disable react/function-component-definition */
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+     Home
+    </div>
+  )
+}
+
+export default Home
