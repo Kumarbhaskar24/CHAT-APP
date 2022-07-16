684b3845bcb6e182ba9b564dd82dae18e43ecae5
Kumar <krbhaskar4321@gmail.com>
2022-07-17 00:40:30 +0530

    created layout for chat page

---
+++ src/components/chat-window/messages/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+
+const Bottom = () => {
+  return <div>messages</div>;
+};
+
+export default Bottom;
