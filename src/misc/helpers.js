6853eeba2f69b22d47e0834c10aef8049c7d7723
Kumar <krbhaskar4321@gmail.com>
2022-07-16 23:26:45 +0530

    Displayed  avatar

---
+++ src/misc/helpers.js
@@ -0,0 +1,11 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/function-component-definition */
+export function getNameInitials(name) {
+    const splitName = name.toUpperCase().split(' ');
+  
+    if (splitName.length > 1) {
+      return splitName[0][0] + splitName[1][0];
+    }
+  
+    return splitName[0][0];
+  }
